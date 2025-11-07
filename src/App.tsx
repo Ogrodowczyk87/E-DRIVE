3cf0c9fcc74526bb085ffec172d529799e871828
Rafal <rafal@Zastrzyk.Zastzyk>
2025-11-07 21:52:01 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navbar } from './components/common/Navbar'
 import { GoogleLoginButton } from './components/auth/GoogleLoginButton'
 import { useGoogleAuth } from './hooks/useGoogleAuth'
