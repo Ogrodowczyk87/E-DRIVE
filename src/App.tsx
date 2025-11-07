5ad40d3f24d4f9f61e387f3849beecfa6d3d2cef
Rafal <rafal@Zastrzyk.Zastzyk>
2025-11-07 21:50:43 +0000

    update Navbar and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-// import Hero from './components/Hero'
 import { Navbar } from './components/common/Navbar'
-
+import { GoogleLoginButton } from './components/auth/GoogleLoginButton'
+import { useGoogleAuth } from './hooks/useGoogleAuth'
 
 export default function App() {
+  const { user } = useGoogleAuth();
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
-        {/* <Hero /> */}
-        <h2 className="text-2xl font-bold text-center mt-10">Welcome to E-Drive</h2>
-        <p className="text-center mt-4">Your one-stop solution for all your file storage needs.</p>
-      </main>
-   
+      {user ? ( 
+        <div>
+          <h2 className="text-2xl font-bold text-center mt-10">Welcome {user?.name}</h2>
+          <p className="text-center mt-4">Your one-stop solution for all your file storage needs.</p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center mt-20">
+          <p className='text-lg text-gray-500'>Please log in to access your files.</p>
+          <GoogleLoginButton />
+        </div>
+      )}
+      </main>  
     </div>
   )
 }
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,10 +1,21 @@
+import { useGoogleAuth } from "../../hooks/useGoogleAuth";
+
 export const Navbar = () => {
+    const { user, logout } = useGoogleAuth();
   return (
     <nav className="w-full bg-white border-b border-gray-200 shadow-sm px-6 py-3 flex justify-between items-center">
       <h1 className="text-xl font-bold text-sky-600">E-Drive</h1>
-      <button className="px-4 py-2 rounded-lg border text-sm hover:bg-gray-100">
-        Sign-in
-      </button>
+        {user ? (
+            <div className="flex items-center gap-4">
+                <img src={user.picture} alt={user.name} className="w-10 h-10 rounded-full" />
+                <span className="text-sm font-medium">{user.name}</span>
+                <button onClick={logout} className="px-4 py-2 rounded-lg border text-sm hover:bg-gray-100">
+                    Sign-out
+                </button>
+            </div>
+        ) : (
+            <span className="text-sm text-gray-500">Not logged in</span>
+        )}
     </nav>
   );
 };
